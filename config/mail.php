36d69131a626bcec8696e6579117d639a6e89a3a
Flores Romero, Eble Christian <46700366+frec0717@users.noreply.github.com>
2022-06-02 05:22:23 -0500

    VERSION ESTABLE

--- config/mail.php
+++ config/mail.php
@@ -78,6 +78,7 @@ return [
                 'log',
             ],
         ],
+        
     ],
 
     /*
@@ -90,6 +91,13 @@ return [
     | used globally for all e-mails that are sent by your application.
     |
     */
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+        ],
+    ],
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
